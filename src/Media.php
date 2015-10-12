7f7b732f11e81c311bcf25f9f5aef087d99c44aa
Johan Björk <johanimon@gmail.com>
2015-10-12 15:00:47 +0200

    Initial commit

---
+++ src/Media.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johan
+ * Date: 15-10-12
+ * Time: 14:51
+ */
+
+namespace Vinnia\Social;
+
+
+class Media {
+
+    const TYPE_TEXT = 'text';
+    const TYPE_IMAGE = 'image';
+    const TYPE_VIDEO = 'video';
+
+    const SOURCE_INSTAGRAM = 'instagram';
+    const SOURCE_TWITTER = 'twitter';
+    const SOURCE_FACEBOOK = 'facebook';
+    const SOURCE_PERISCOPE = 'periscope';
+
+    /**
+     * @var string
+     */
+    public $source;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $data;
+
+    /**
+     * @var string
+     */
+    public $caption;
+
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var int
+     */
+    public $createdAt;
+
+}
---
+++ test/InstagramSearchTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johan
+ * Date: 15-10-12
+ * Time: 14:55
+ */
+
+namespace Vinnia\Social\Test;
+
+
+class InstagramSearchTest extends \PHPUnit_Framework_TestCase {
+
+    public function testSearchByTag() {
+        $this->fail('Wow such fail');
+    }
+
+    public function testSearchByUsername() {
+        $this->fail('Wow very bad');
+    }
+
+}
