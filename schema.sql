337110d7f1070d04ed5866115eaf98bb5c902b66
Johan Björk <johanimon@gmail.com>
2015-10-20 19:19:27 +0200

    Fix unique id collisions

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "vss_media" (
   "vss_media_id" int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   "source" tinyint not null,
-  "original_id" varchar(100) not null unique,
+  "original_id" varchar(100) not null,
   "text" text,
   "images" text,
   "videos" text,
@@ -11,6 +11,7 @@ CREATE TABLE "vss_media" (
   "created_at" int(11) NOT NULL
 );
 
+create unique index vss_media_source_original_id_idx on vss_media("source", "original_id");
 create index vss_media_created_at_idx on vss_media("created_at");
 create index vss_media_lat_idx on vss_media("lat");
 create index vss_media_long_idx on vss_media("long");
